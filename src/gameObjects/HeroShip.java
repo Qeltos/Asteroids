97a4ab92bbdd0ffda2b2c6c56a6b67739cf21ed6
Mikael <mikael_wedin@hotmail.com>
2025-07-25 21:55:28 +0200

    Added functionality: HeroShip.java, Main.java
    added rotation to HeroShip.java and controls for rotation to Main.java

--- src/gameObjects/HeroShip.java
+++ src/gameObjects/HeroShip.java
@@ -6,9 +6,11 @@ import java.util.ArrayList;
 import utils.Node;
 
 public class HeroShip {
-	
+
 	private ArrayList<Node> nodes;
-	
+	private int centerX = 400, centerY = 400, shipSize = 30;
+	private double rotation, rad;
+
 	public HeroShip() {
 		nodes = new ArrayList<Node>();
 		generateShape();
@@ -17,24 +19,30 @@ public class HeroShip {
 	private void generateShape() {
 		int angleStep = 360 / 3;
 		for (int i = 0; i < 3; i++) {
-			double angle = Math.toRadians(i * angleStep - 90);
-            int px = (int) (Math.cos(angle) * 120) + 400;
-            int py = (int) (Math.sin(angle) * 120) + 400;
-            nodes.add(new Node(px, py));
+			rad = Math.toRadians(i * angleStep - 90);
+			int px = (int) (Math.cos(rad) * shipSize);
+			int py = (int) (Math.sin(rad) * shipSize);
+			nodes.add(new Node(px, py));
 		}
-		
-		for(Node n : nodes) {
-			System.out.println(n);
-		}
-    }
-	
+	}
+
 	public void draw(Graphics g) {
-		if(nodes.size() >= 2) {
+
+		if (nodes.size() >= 2) {
 			Polygon p = new Polygon();
 			for (Node n : nodes) {
-			    p.addPoint(n.getX(), n.getY());
+				
+				int xRotated = (int) (n.getX() * Math.cos(rotation) - n.getY() * Math.sin(rotation));
+				int yRotated = (int) (n.getX() * Math.sin(rotation) + n.getY() * Math.cos(rotation));
+
+				p.addPoint(centerX + xRotated, centerY + yRotated);
 			}
-			g.drawPolygon(p);		
+			g.drawPolygon(p);
 		}
+
+	}
+
+	public void rotate(int rotation) {
+		this.rotation += Math.toRadians(rotation);
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -3,13 +3,15 @@ package main;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import gameObjects.HeroShip;
 
-public class Main extends JPanel{
+public class Main extends JPanel implements KeyListener {
 	
 	/**
 	 * 
@@ -21,16 +23,22 @@ public class Main extends JPanel{
 	public Main() {
 		setPreferredSize(new Dimension(HEIGHT, WIDTH));
 		setBackground(Color.BLACK);
+		
+		JFrame frame = new JFrame("Asteroids");
+		
+		frame.add(this);
+		frame.addKeyListener(this);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
 		hero = new HeroShip();
 	}
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Asteroids");
+		
 		Main game = new Main();
-		frame.add(game);
-		frame.pack();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+
 	}
 	
 	@Override
@@ -38,6 +46,34 @@ public class Main extends JPanel{
 		super.paintComponent(g);
 		g.setColor(Color.white);
 		hero.draw(g);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		System.out.println(e.getKeyChar());
+		
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			hero.rotate(5);
+			repaint();
+		}
+		
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			hero.rotate(-5);
+			repaint();
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }

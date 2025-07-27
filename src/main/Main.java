ced5bca06ad377fe4d3dff0f7bd2de8f6f5ae921
Mikael Wedin <mikael_wedin@hotmail.com>
2025-07-28 00:36:38 +0200

    Refactor: Main.java
    Added timer and actionlistener for cleaner code.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,79 +1,72 @@
 package main;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import gameObjects.HeroShip;
 
-public class Main extends JPanel implements KeyListener {
-	
-	/**
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main extends JPanel implements ActionListener, KeyListener {
+    /**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private HeroShip hero;
-	private final int WIDTH = 800, HEIGHT = 800;
-	
-	public Main() {
-		setPreferredSize(new Dimension(HEIGHT, WIDTH));
-		setBackground(Color.BLACK);
-		
-		JFrame frame = new JFrame("Asteroids");
-		
-		frame.add(this);
-		frame.addKeyListener(this);
-		frame.pack();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-		
-		hero = new HeroShip();
-	}
+	private HeroShip ship;
+    private Timer timer;
+    private final int WIDTH = 800, HEIGHT = 600;
+    
+    public Main() {
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setBackground(Color.BLACK);
+        ship = new HeroShip(WIDTH / 2, HEIGHT / 2);
+        
+        timer = new Timer(16, this); // 60 FPS (1000ms / 60 ≈ 16ms per frame)
+        timer.start();
+        
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+    }
 
-	public static void main(String[] args) {
-		
-		Main game = new Main();
+   
 
-	}
-	
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		g.setColor(Color.white);
-		hero.draw(g);
-	}
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.WHITE);
+        ship.draw(g);
+    }
 
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        ship.move();
+        repaint();
+    }
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		System.out.println(e.getKeyChar());
-		
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-			hero.rotate(5);
-			repaint();
-		}
-		
-		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-			hero.rotate(-5);
-			repaint();
-		}
-		
-	}
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            ship.rotate(-5);
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            ship.rotate(5);
+        } else if (e.getKeyCode() == KeyEvent.VK_UP) {
+            ship.accelerate();
+        }
+    }
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void keyReleased(KeyEvent e) {}
 
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Asteroids Game");
+        Main game = new Main();
+        frame.add(game);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }

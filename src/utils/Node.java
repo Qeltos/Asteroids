18b44b0aa3a91c342d23534fb879fdb8880a48d8
Mikael Wedin <mikael_wedin@hotmail.com>
2025-07-28 00:27:40 +0200

    Refactor: Node.js
    changed from integer to double in favor for more precision

--- src/utils/Node.java
+++ src/utils/Node.java
@@ -1,27 +1,27 @@
 package utils;
 
 public class Node {
-	
-	private int x,y;
-	
-	public Node(int x, int y) {
+
+	private double x, y;
+
+	public Node(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
 
-	public int getX() {
+	public double getX() {
 		return x;
 	}
 
-	public void setX(int x) {
+	public void setX(double x) {
 		this.x = x;
 	}
 
-	public int getY() {
+	public double getY() {
 		return y;
 	}
 
-	public void setY(int y) {
+	public void setY(double y) {
 		this.y = y;
 	}
 }

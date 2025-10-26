f5a777de4bba7afc20d8b8f0a9fb2e18958b9555
Mikael <mikael_wedin@hotmail.com>
2025-10-26 14:43:22 +0100

    Refactor: Main.java; added gameObject list and asteroid spawntimer.

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,52 +8,60 @@ import gameObjects.HeroShip;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 public class Main extends JPanel implements ActionListener, KeyListener {
     /**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private ArrayList<GameObject> gameObjects = new ArrayList<>();
 	private GameObject ship;
-	private GameObject asteroid;
     private Timer timer;
+    private Timer asteroidSpawnTimer;
     private final int WIDTH = 800, HEIGHT = 600;
     
     public Main() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setBackground(Color.BLACK);
-        ship = new HeroShip(WIDTH / 2, HEIGHT / 2);
-        asteroid = new Asteroid(150,150);
         
+        ship = new HeroShip(WIDTH / 2, HEIGHT /2) ;
+        gameObjects.add(ship);
         
         timer = new Timer(16, this); // 60 FPS (1000ms / 60 ≈ 16ms per frame)
+        asteroidSpawnTimer = new Timer(5000, x -> {
+        	gameObjects.add(new Asteroid(150,150));
+        }) ;
         timer.start();
+        asteroidSpawnTimer.start();
         
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
         
     }
-
+    
+    public void spawnAsteroids() {
+    	
+    }
    
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.WHITE);
-        ship.draw(g);
-        asteroid.draw(g);
+        
+        for (GameObject o : gameObjects) {
+        	o.draw(g);
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        ship.move();
-        asteroid.move();
-        
-		if (ship.collided(asteroid)) {
-			System.out.println("we have collision");
-		}
-        
+    	for (GameObject o : gameObjects) {
+    		o.move();
+    	}
+                
         repaint();
     }
 

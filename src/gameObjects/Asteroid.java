5148c5122b4b67675d34835120947d51d0e4a1f9
Mikael <mikael_wedin@hotmail.com>
2026-04-20 11:34:53 +0200

    Feat: Asteroid sizes
    Asteroids now have three new sizes

--- src/gameObjects/Asteroid.java
+++ src/gameObjects/Asteroid.java
@@ -5,6 +5,10 @@ import java.util.Random;
 import utils.Node;
 
 public class Asteroid extends GameObject {
+	
+	private final int SMALL = 10;
+	private final int MEDIUM = 25;
+	private final int LARGE = 50;
 
 	public Asteroid(double x, double y) {
 		this(new Node(x, y));
@@ -13,16 +17,36 @@ public class Asteroid extends GameObject {
 	public Asteroid(Node spawnPoint) {
 		super(spawnPoint);
 		Random rand = new Random();
-		radius = 100;
+		radius = size(rand.nextInt(1, 4));
 		rotation = rand.nextDouble(-1, 1);
 		nrOfNodes = rand.nextInt(7, 17);
 		generateShape();
 	}
 
+	private int size(int key) {
+		int value = 0;
+		System.out.println(key);
+
+		switch (key) {
+		case 1: {
+			value = SMALL;
+			break;
+		}
+		case 2: {
+			value =  MEDIUM;
+			break;
+		}
+		case 3: {
+			value =  LARGE;
+			break;
+		}
+		}
+		return value;
+	}
+
 	@Override
 	public void onCollision(Collidable object) {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
@@ -30,11 +54,11 @@ public class Asteroid extends GameObject {
 		int angleStep = 360 / nrOfNodes;
 		for (int i = 0; i < nrOfNodes; i++) {
 			radians = Math.toRadians(i * angleStep);
-			double px = Math.cos(radians) * radius;
-			double py = Math.sin(radians) * radius;
+			double offset = (Math.random() - 0.5) * radius * 0.4;
+			double px = Math.cos(radians) * radius + offset;
+			double py = Math.sin(radians) * radius + offset;
 			nodes.add(new Node(px, py));
-		}		
+		}
 	}
-	
-	
+
 }
